4d3c906ac234364b8a48fed1d6c8c2ae3929919a
James Davey <james.t.davey@gmail.com>
2018-11-02 11:18:43 -0700

    fix bug that didn't generate correct number of pages for blog

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,14 +1,15 @@
-import nav from "./modules/nav.js";
+import nav from './modules/nav.js';
 
-import BlogPost from "./modules/BlogPost.js";
-import BlogPage from "./modules/BlogPage.js";
-import Blog from "./modules/Blog.js";
+import BlogPost from './modules/BlogPost.js';
+import BlogPage from './modules/BlogPage.js';
+import Blog from './modules/Blog.js';
 
-const blogContainerEl = document.querySelector(".blog__container");
+const blogContainerEl = document.querySelector('.blog__container');
 
-const API_KEY = "GPFs9wx2Y6jaEyhsxfrhXqcbYlmGefKkngraQ7jOUH5xFmXZy1";
-const URL_ENDPOINT = "https://api.tumblr.com";
+const API_KEY = 'GPFs9wx2Y6jaEyhsxfrhXqcbYlmGefKkngraQ7jOUH5xFmXZy1';
+const URL_ENDPOINT = 'https://api.tumblr.com';
 let url = `${URL_ENDPOINT}/v2/blog/jamesthomasdavey.tumblr.com/posts/text?api_key=${API_KEY}`;
+const postsPerPage = 4;
 let blogPosts;
 let blogPages = [];
 let myBlog;
@@ -27,9 +28,11 @@ fetch(url)
   });
 
 const makePages = () => {
-  for (let i = 0; i < (blogPosts.length % 5) + 1; i++) {
-    blogPages.push(new BlogPage(blogPosts.splice(0, 5)));
+  const numberOfPages = Math.ceil(blogPosts.length / postsPerPage);
+  for (let i = 0; i < numberOfPages; i++) {
+    blogPages.push(new BlogPage(blogPosts.splice(0, postsPerPage)));
   }
+  console.log(blogPages);
 };
 
 const makeBlog = () => {
@@ -37,12 +40,11 @@ const makeBlog = () => {
 };
 
 const renderPosts = () => {
-  blogContainerEl.classList.remove("display");
+  blogContainerEl.classList.remove('display');
   setTimeout(() => {
     let markup = ``;
-    myBlog.pages[myBlog.currentPageIndex].posts.forEach(
-      (post, index, array) => {
-        markup += `
+    myBlog.pages[myBlog.currentPageIndex].posts.forEach((post, index, array) => {
+      markup += `
         <div class="blog__post">
           <div class="blog__post-heading">
             <h2 class="blog__post-title">${post.title}</h2>
@@ -53,33 +55,30 @@ const renderPosts = () => {
           </div>
         </div>
       `;
-        markup +=
-          index + 1 === array.length
-            ? ``
-            : `
+      markup +=
+        index + 1 === array.length
+          ? ``
+          : `
         <div class="blog__post-divider"></div>
       `;
-      }
-    );
+    });
 
     let footerMarkup = `
       <div class="blog__page-footer">
         <button class="blog__arrow blog__arrow-future" ${
-          myBlog.isOnFirstPage() ? "disabled" : null
+          myBlog.isOnFirstPage() ? 'disabled' : null
         }>
           <div class="blog__arrow-bar blog__arrow-future-bar-1"></div>
           <div class="blog__arrow-bar blog__arrow-future-bar-2"></div>
         </button>
-        <button class="blog__arrow blog__arrow-past" ${
-          myBlog.isOnLastPage() ? "disabled" : null
-        }>
+        <button class="blog__arrow blog__arrow-past" ${myBlog.isOnLastPage() ? 'disabled' : null}>
           <div class="blog__arrow-bar blog__arrow-past-bar-1"></div>
           <div class="blog__arrow-bar blog__arrow-past-bar-2"></div>
         </button>
       </div>
     `;
     blogContainerEl.innerHTML = markup + footerMarkup;
-    blogContainerEl.classList.add("display");
+    blogContainerEl.classList.add('display');
   }, 200);
 };
 
@@ -91,14 +90,14 @@ const scrollToTop = () => {
   } else clearTimeout(timeOut);
 };
 
-document.querySelector("body").addEventListener("click", e => {
-  if (e.target.classList.contains("blog__arrow-future")) {
+document.querySelector('body').addEventListener('click', e => {
+  if (e.target.classList.contains('blog__arrow-future')) {
     myBlog.decreasePageIndex();
     renderPosts();
     setTimeout(() => {
       scrollToTop();
     }, 300);
-  } else if (e.target.classList.contains("blog__arrow-past")) {
+  } else if (e.target.classList.contains('blog__arrow-past')) {
     myBlog.advancePageIndex();
     renderPosts();
     setTimeout(() => {
